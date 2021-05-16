866f8fead9459318edb2ec8616f851eabda5483f
김태주 <kim9xd@gmail.com>
2021-05-16 18:04:55 +0900

    Stream API

--- src/main/java/me/pulpury/java8to11/App.java
+++ src/main/java/me/pulpury/java8to11/App.java
@@ -1,71 +1,86 @@
 package me.pulpury.java8to11;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collection;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
 	
 	public static void main(String[] args) {
-//		Foo2 foo = new DefaultFoo("taeju");
-////		Bar foo = new DefaultFoo("taeju");
-//		foo.printName();
-//		foo.printNameUpperCase();
-//		
-//		Foo2.printAnything();
-//		
 		
-		List<String> names =  new ArrayList<>();
-		names.add("taeju");
-		names.add("pulpury");
-		names.add("toby");
-		names.add("foo");
-		
-//		names.forEach(System.out::println);
-//		
-//		// 위의 경우와 똑같다.
-//		for (String n : names) {
-//			System.out.println(n);
-//		}
-//		
-//		Spliterator<String> spliterator = names.spliterator();
-//		Spliterator<String> spliterator1 = spliterator.trySplit();
-//		while (spliterator.tryAdvance(System.out::println));
-//		System.out.println("============");
-//		while (spliterator1.tryAdvance(System.out::println));
-		
-//		long k = name.stream().map(String::toUpperCase));
-		
-		Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
-//		name.sort(compareToIgnoreCase.reversed().thenComparing());
-		
-		List<String> collect = names.stream().map((s) -> {
-			System.out.println(s);
-			return s.toUpperCase();
-		}).collect(Collectors.toList());
-		
-		collect.forEach(System.out::println);
-		
-		System.out.println("============");
-		
-		names.forEach(System.out::println);
-		
-		System.out.println("============");
-		
-		// Stream과 다르게
-		// parallelStream는 병렬적인 구조 처리가 가능함.(쓰레기가 다름, Tread safe 하지 않다.)
-		// parallelStream는 데이터가 정말 방대한 경우(아주 드문경우) 유용함.
-		// 일반적인 경우 Stream이 유용.
-		// 그러나 성능 차이는 직접 테스트해서 따져봐야 함.
-		List<String> collect1 = names.parallelStream().map((s) -> {
-			System.out.println(s + " " + Thread.currentThread().getName());
-			return s.toUpperCase();
-		})
-				.collect(Collectors.toList());
-		
-		collect1.forEach(System.out::println);
-		
+		List<OnlineClass> springClasses = new ArrayList<>();
+        springClasses.add(new OnlineClass(1, "spring boot", true));
+        springClasses.add(new OnlineClass(2, "spring data jpa", true));
+        springClasses.add(new OnlineClass(3, "spring mvc", false));
+        springClasses.add(new OnlineClass(4, "spring core", false));
+        springClasses.add(new OnlineClass(5, "rest api development", false));
+
+        List<OnlineClass> javaClasses = new ArrayList<>();
+        javaClasses.add(new OnlineClass(6, "The Java, Test", true));
+        javaClasses.add(new OnlineClass(7, "The Java, Code manipulation", true));
+        javaClasses.add(new OnlineClass(8, "The Java, 8 to 11", false));
+
+        List<List<OnlineClass>> keesunEvents = new ArrayList<>();
+        keesunEvents.add(springClasses);
+        keesunEvents.add(javaClasses);
+
+        System.out.println("spring 으로 시작하는 수업");
+        springClasses.stream()
+        	.filter(oc -> oc.getTitle().startsWith("spring"))
+        	.forEach(oc -> System.out.println(oc.getId()));
+        
+        System.out.println("=======================================");
+        
+        System.out.println("close 되지 않은 수업");
+        springClasses.stream().filter(oc -> !oc.isClosed())
+        	.forEach(oc -> System.out.println(oc.getId()));
+        
+        springClasses.stream()
+        	.filter(Predicate.not(OnlineClass::isClosed))
+        	.forEach(oc -> System.out.println(oc.getId()));
+        
+        System.out.println("=======================================");
+        
+        System.out.println("수업 이름만 모아서 스트림 만들기");
+        springClasses.stream()
+        	.map(oc -> oc.getTitle())
+//        	.forEach(s -> System.out.println(s));
+        	.forEach(System.out::println);
+        
+        springClasses.stream()
+    	.map(OnlineClass::getTitle)
+    	.forEach(System.out::println);
+        
+        System.out.println("=======================================");
+
+        System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        keesunEvents.stream().flatMap(list -> list.stream())
+        			.forEach(oc -> System.out.println(oc.getId()));
+        keesunEvents.stream().flatMap(Collection::stream)
+					.forEach(oc -> System.out.println(oc.getId()));
+
+        System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에 10개 빼고 최대 10개 까지만");
+        Stream.iterate(10, i -> i + 1)
+        	.skip(10)
+        	.limit(10)
+//        	.forEach(i -> System.out.println(i));
+        	.forEach(System.out::println);
+
+        System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        boolean test = javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println(test);
+
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 것만 모아서 List로 만들기");
+        List<String> spring = springClasses.stream()
+        		.filter(oc -> oc.getTitle().contains("spring"))
+        		.map(OnlineClass::getTitle)
+        		.collect(Collectors.toList());
+        spring.forEach(System.out::println);
+        spring.forEach(s -> System.out.println(s));
+        
 		
 	}
 }
