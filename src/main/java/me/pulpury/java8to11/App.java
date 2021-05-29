e3c6ebed9c99e4c9f99ba17ca6bcbcd64a7f4a80
김태주 <kim9xd@gmail.com>
2021-05-29 14:41:12 +0900

    애노테이션의 변화

--- src/main/java/me/pulpury/java8to11/App.java
+++ src/main/java/me/pulpury/java8to11/App.java
@@ -2,102 +2,36 @@ package me.pulpury.java8to11;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
+@Chicken("양념")
+@Chicken("마늘간장")
 public class App {
 	
-	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> {
-			System.out.println("Hello " + Thread.currentThread().getName());
-			return "Hello";
+	// @Target(ElementType.TYPE_USE)를 사용하면 type을 사용하는 모든 곳에서
+	// 사용할 수 있다.
+//	public static void main(@Chicken String[] args) throws @Chicken RuntimeException {
+	public static void main(String[] args) throws RuntimeException {
+//		List<@Chicken String> names = Arrays.asList("Taeju");
+		
+		Chicken[] chickens = App.class.getAnnotationsByType(Chicken.class);
+		Arrays.stream(chickens).forEach(c -> {
+			System.out.println(c.value());
 		});
 		
-		CompletableFuture<String> future = hello.thenCompose(App::getWorld);
-		
-		System.out.println(future.get());
-		
-		CompletableFuture<String> hello2 = CompletableFuture.supplyAsync(() -> {
-			System.out.println("Hello2 " + Thread.currentThread().getName());
-			return "Hello2";
+		// Chicken타입이 아닌 ChickenContainer 타입으로도 가져올 수 있다.!
+		ChickenContainer chickenContainer = App.class.getAnnotation(ChickenContainer.class);
+		Arrays.stream(chickenContainer.value()).forEach(c -> {
+			System.out.println(c.value());
 		});
-		
-		CompletableFuture<String> world2 = CompletableFuture.supplyAsync(() -> {
-			System.out.println("World2 " + Thread.currentThread().getName());
-			return "World2";
-		});
-		
-		CompletableFuture<String> future2 = hello2.thenCombine(world2, (h, w) -> h + " " + w);
-		System.out.println(future2.get());
-		
-		// 아래는 null 값이 나온다.
-		CompletableFuture<Void> future3 = CompletableFuture.allOf(hello2, world2)
-					.thenAccept((result) -> {
-						System.out.println(result);
-					});
-		
-		System.out.println(future3.get());
-		
-		List<CompletableFuture> futures = Arrays.asList(hello2, future2);
-		
-		CompletableFuture<Void> futureArray = CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
-		
-		CompletableFuture<List<Object>> results = CompletableFuture.allOf(futureArray)
-				.thenApply((v) -> {
-					return futures.stream()
-							.map(CompletableFuture::join)
-							.collect(Collectors.toList());
-				});
-		
-		results.get().forEach(System.out::println);
-		
-		System.out.println(results.get());
-		
-		CompletableFuture<Void> future4 = CompletableFuture.anyOf(hello2, world2).thenAccept((s) -> {
-			System.out.println(s);
-		});
-		
-		future4.get();
-		
-		boolean throwError = true;
-		
-		CompletableFuture<String> hello5 = CompletableFuture.supplyAsync(() -> {
-			if (throwError) {
-				throw new IllegalArgumentException();
-			}
-			
-			System.out.println("Hello5 " + Thread.currentThread().getName());
-			return "Hello";
-		}).exceptionally(ex -> {
-			return "Error!";
-		});
-		
-		CompletableFuture<String> hello6 = CompletableFuture.supplyAsync(() -> {
-			if (throwError) {
-				throw new IllegalArgumentException();
-			}
-			
-			System.out.println("Hello5 " + Thread.currentThread().getName());
-			return "Hello";
-		}).handle((result, ex) -> {
-			if (ex != null) {
-				System.out.println(ex);
-				return "ERROR!";
-			}
-			
-			return result;
-		});
-		
-		System.out.println(hello6.get());
-		
 	}
 	
-	private static CompletableFuture<String> getWorld(String message) {
-		return CompletableFuture.supplyAsync(() -> {
-			System.out.println("World " + Thread.currentThread().getName());
-			return message + " World";
-		});
-	}
+	// @Target(ElementType.TYPE_PARAMETER) 덕분에 사용이 가능하다.
+//	static class FeelsLikeChichichen<@Chicken T> {
+		
+//		public static <@Chicken C> void print(@Chicken C c) {
+//			System.out.println(c);
+//		}
+		
+//	}
 	
 }
